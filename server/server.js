118cd4d82b3d07167afb9b416e7af585ba84fd2c
spencercreer <spencercreer@gmail.com>
2021-10-27 19:01:41 -0600

    typeDefs

--- server/server.js
+++ server/server.js
@@ -2,10 +2,25 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express')
+
+const { typeDefs, resolvers } = require('./schemas')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+let apollo_server;
+async function startServer() {
+  apollo_server = new ApolloServer({
+    typeDefs,
+    resolvers
+  });
+  
+  apollo_server.applyMiddleware({ app });
+}
+
+startServer();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,8 +30,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`)
+    console.log(`Use GrqphQL at http://localhost:${PORT}${server.graphqlPath}`)
+  });
 });
